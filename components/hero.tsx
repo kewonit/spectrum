15fc25b656d8478d4a03ee7231401882cdc31090
Mayank kadam <mayankkadam1039@gmail.com>
2025-02-05 23:50:11 +0530

    landing page bug fix

--- components/hero.tsx
+++ components/hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
 
   return (
     <div className="hero-container grid-background-flip">  {/* Add this wrapper class */}
-      <div className="container">
+      <div className="container-out">
         <div className="content">
           <div className="container-full">
             <div className="animated hue"></div>
