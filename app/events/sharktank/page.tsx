403fc51785e0d9d30a77894ce2c50f0ff3ed4373
Aadithyan Rajesh <nair.aadithyan999@gmail.com>
2025-03-06 14:02:43 +0530

    (feat): not found + images

--- app/events/sharktank/page.tsx
+++ app/events/sharktank/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     siteName: "spectrumpccoe25.tech",
     images: [
       {
-        url: "https://i.postimg.cc/gk5vZzQc/4.png",
+        url: "https://res.cloudinary.com/dr38lv00a/image/upload/v1741248707/yobgkxnxtbn64ylifkew.png",
         width: 548,
         height: 253,
         alt: 'Image',
@@ -73,7 +73,7 @@ export default function Home() {
             </div>
             <div className="w-full md:w-1/2 flex justify-center">
               <Image 
-                src="https://i.postimg.cc/gk5vZzQc/4.png" 
+                src="https://res.cloudinary.com/dr38lv00a/image/upload/v1741248707/yobgkxnxtbn64ylifkew.png" 
                 alt="Event Image" 
                 width={400} 
                 height={400} 
